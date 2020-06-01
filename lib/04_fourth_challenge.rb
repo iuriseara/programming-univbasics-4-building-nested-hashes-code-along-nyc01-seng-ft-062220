14808dac94e2db5a1ad47c2b427f755c4fa45c98
fearless-command-2358 <iuriseara@gmail.com>
2020-06-01 15:52:49 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -7,14 +7,14 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
         {
-        :name => "Benvolio",
-        :age => "17",
-        :attitude => "worried"
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         },
         {
-        :name => "Steven",
-        :age => "30",
-        :attitude => "confused"
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
         }
       ]
    },
@@ -24,7 +24,11 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
       ]
    }
 }
